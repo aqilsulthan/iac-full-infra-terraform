088a4c7a6776ea913591515aeee6b764d5d1e77a
aqilsulthan <aqilsulthaan@gmail.com>
2025-12-24 14:02:48 +0700

    update alb and igw

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -18,3 +18,27 @@ resource "aws_subnet" "public" {
     Name = "public-subnet-${count.index}"
   }
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.azs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
