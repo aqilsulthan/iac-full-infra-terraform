39db6b1366f262ef7b969649eee5e30f1fe07205
aqil <aqilsulthaan@gmail.com>
2026-04-28 14:15:43 +0700

    feat(secrets): add recovery windows variable to prevent accidental permanent deletion

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -1,5 +1,6 @@
 resource "aws_secretsmanager_secret" "secret" {
-  name = var.secret_name
+  name                    = var.secret_name
+  recovery_window_in_days = var.recovery_window_in_days
 }
 
 resource "aws_secretsmanager_secret_version" "secret_version" {
--- modules/secrets/variables.tf
+++ modules/secrets/variables.tf
@@ -1,2 +1,7 @@
 variable "secret_name" { type = string }
 variable "secret_string" { type = string }
+variable "recovery_window_in_days" {
+  description = "Jumlah hari secret disimpan sebelum dihapus permanen. Set ke 0 untuk menghapus langsung (berguna di Dev)."
+  type        = number
+  default     = 0
+}
